ffa64faa6d4ebe8c57fcfd1726a962faf58bdbe2
effirin <effirin8@gmail.com>
2026-03-06 02:32:43 +0900

    dingcorithm > GetCountOfWaysToTargetByDoingPlusOrMinus 클래스 및 알고리즘 문제 풀이 추가

---
+++ src/main/kotlin/dingcorithm/GetCountOfWaysToTargetByDoingPlusOrMinus.kt
@@ -0,0 +1,55 @@
+package dingcorithm
+
+/***
+ * DFS + 재귀 함수
+ * 시간 복잡도 : O(2^N)
+ * 공간 복잡도 : O(N) -> 재귀 깊이 만큼 콜스택이 쌓이니까
+ */
+fun main() {
+    val numbers = intArrayOf(1, 1, 1, 1, 1)
+    val targetNumber = 3
+
+    println(GetCountOfWaysToTargetByDoingPlusOrMinus().getCountOfWaysToTargetByDoingPlusOrMinus(numbers, targetNumber)) // 5
+}
+
+class GetCountOfWaysToTargetByDoingPlusOrMinus {
+
+    val allWays = ArrayList<Int>()
+
+    fun getCountOfWaysToTargetByDoingPlusOrMinus(numbers: IntArray, targetNumber: Int): Int {
+
+        // 맨 처음에 시작할 재귀함수의 시작점은 여기서 준다.
+        getAllWaysByDoingPlusOrMinus(
+            currentIndex = 0,
+            currentSum = 0,
+            array = numbers
+        )
+
+        var targetCount = 0
+
+        for (way in allWays) {
+            if(way == targetNumber) targetCount++
+        }
+
+        return targetCount
+    }
+
+    private fun getAllWaysByDoingPlusOrMinus(currentIndex: Int, currentSum: Int, array: IntArray) {
+        if(currentIndex == array.size) {
+            allWays.add(currentSum)
+            return
+        }
+
+        getAllWaysByDoingPlusOrMinus(
+            currentIndex = currentIndex + 1,
+            currentSum = currentSum + array[currentIndex],
+            array = array
+        )
+
+        getAllWaysByDoingPlusOrMinus(
+            currentIndex = currentIndex + 1,
+            currentSum = currentSum - array[currentIndex],
+            array = array
+        )
+    }
+}
